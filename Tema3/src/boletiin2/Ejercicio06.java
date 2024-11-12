2310bcd79d00d4461c45eecc0bd174937e368fdb
elenarivero <145675391+elenarivero@users.noreply.github.com>
2024-11-12 14:06:56 +0100

    Ejercicio 6 corregido

---
+++ Tema3/src/boletiin2/Ejercicio06.java
@@ -0,0 +1,43 @@
+package boletiin2;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Ejercicio06 {
+
+	public static void main(String[] args) {
+		//10, 9, 7, 5, 2  || 2, 5, 7, 9, 10
+		//-5,-4,-3,-2,-1
+		int[] ganadora = {2,5,7,9,10,13};
+		int[] apuesta = new int[6];
+		
+		Random rand = new Random();
+		int cont = 0;
+		int numAleatorio;
+		int busqueda;
+		int numAciertos = 0;
+		
+		while(cont<6) {
+			numAleatorio = rand.nextInt(1, 50);
+			Arrays.sort(apuesta);
+			busqueda = Arrays.binarySearch(apuesta, numAleatorio);
+			if(busqueda < 0) {
+				apuesta[0] = numAleatorio;
+				cont++;
+			}
+		}
+		
+		System.out.println(Arrays.toString(apuesta));
+		
+		for(int valor: apuesta) {
+			busqueda = Arrays.binarySearch(ganadora, valor);
+			if(busqueda>=0) {
+				numAciertos++;
+			}
+		}
+		
+		System.out.println("Número de aciertos: " + numAciertos);
+		//System.out.println(Arrays.binarySearch(ganadora, 11));
+	}
+
+}

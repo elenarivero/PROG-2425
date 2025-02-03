daf64889bc60d12091d9cf0d2d37733f195005eb
elenarivero <145675391+elenarivero@users.noreply.github.com>
2025-02-03 11:06:12 +0100

    Tema 4 - Orientación a objetos

---
+++ Tema4/src/clases/Persona.java
@@ -0,0 +1,11 @@
+package clases;
+
+public class Persona {
+	String nombre = "";
+	int edad;
+	double estatura;
+	String DNI;
+	String diaSemana;
+	
+
+}
---
+++ Tema4/src/clases/Principal.java
@@ -0,0 +1,18 @@
+package clases;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Persona per1 = new Persona();
+		
+		per1.nombre = "Paco";
+		per1.edad = 18;
+		System.out.println(per1.nombre);
+		
+		Persona per2 = per1;
+		per2.nombre = "Pepe";
+		System.out.println(per1.nombre);
+		
+	}
+
+}

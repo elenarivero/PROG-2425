8865eec720765f49782e6d0c0c881de977b097ee
elenarivero <145675391+elenarivero@users.noreply.github.com>
2025-02-03 12:03:08 +0100

    Ejercicio del Punto terminado

--- Tema4/src/parte1/ejercicio1/Punto.java
+++ Tema4/src/parte1/ejercicio1/Punto.java
@@ -2,5 +2,14 @@ package parte1.ejercicio1;
 
 public class Punto {
 
+	/**
+	 * Coordenada x del punto
+	 */
+	int x;
+	
+	/**
+	 * Coordenada y del punto
+	 */
+	int y;
 	
 }
--- Tema4/src/parte1/ejercicio1/PuntoPrincipal.java
+++ Tema4/src/parte1/ejercicio1/PuntoPrincipal.java
@@ -3,8 +3,42 @@ package parte1.ejercicio1;
 public class PuntoPrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Creamos 3 puntos
+		Punto p1 = new Punto();
+		Punto p2 = new Punto();
+		Punto p3 = new Punto();
 
+		// Coordenadas del punto 1
+		p1.x = 5;
+		p1.y = 0;
+
+		// Coordenadas del punto 2
+		p2.x = 10;
+		p2.y = 10;
+
+		// Coordenadas del punto 3
+		p3.x = -3;
+		p3.y = 7;
+
+		// Mostramos por pantalla los 3 puntos
+		System.out.println("Punto 1: (" + p1.x + ", " + p1.y + ")");
+		System.out.println("Punto 2: (" + p2.x + ", " + p2.y + ")");
+		System.out.println("Punto 3: (" + p3.x + ", " + p3.y + ")");
+
+		// Realizamos operaciones varias sobre los puntos
+		p1.x += 2;
+		p1.y -= 3;
+
+		p2.x *= 2;
+		p2.y = 5;
+
+		p3.x /= 3;
+		p3.y %= 5;
+		
+		// Volvemos a imprimir los puntos
+		System.out.println("Punto 1: (" + p1.x + ", " + p1.y + ")");
+		System.out.println("Punto 2: (" + p2.x + ", " + p2.y + ")");
+		System.out.println("Punto 3: (" + p3.x + ", " + p3.y + ")");
 	}
 
 }

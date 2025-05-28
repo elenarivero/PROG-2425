ae6c698e446bc7571d618e7be9a40d670fe298b2
elenarivero <145675391+elenarivero@users.noreply.github.com>
2025-05-28 08:38:10 +0200

    Comprobación de fila y columna introducida por el jugador en el hundir la flota.

--- ExamenHundirFlota/src/hundir/HundirFlota.java
+++ ExamenHundirFlota/src/hundir/HundirFlota.java
@@ -1,10 +1,35 @@
 package hundir;
 
+import java.util.Scanner;
+
 public class HundirFlota {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static Scanner reader = new Scanner(System.in);
 
+	public static void main(String[] args) {
+		char[][] tablero = new char[4][4];
+		char[][] tableroJugador = new char[4][4];
+		turnoJugador(tablero, tableroJugador);
+	}
+
+	static boolean turnoJugador(char tablero[][], char tableroJugador[][]) {
+		boolean res = false;
+		char fila;
+		int columna;
+		int pos;
+
+		do {
+			System.out.println("Introduzca una fila como letra:");
+			fila = reader.next().toUpperCase().charAt(0);
+			pos = (fila - 'A');
+		} while (pos < 0 || pos >= tablero.length);
+
+		do {
+			System.out.println("Introduzca una columna como número:");
+			columna = reader.nextInt();
+		} while (columna < 0 || columna >= tablero.length);
+
+		return res;
 	}
 
 }

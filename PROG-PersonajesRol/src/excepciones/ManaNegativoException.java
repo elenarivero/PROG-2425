14ba3d1105f6eee0c94eff23ac8bcaf6f66cb8e8
elenarivero <145675391+elenarivero@users.noreply.github.com>
2025-06-12 12:25:57 +0200

    Se ha añadido el toString a las excepciones.

--- PROG-PersonajesRol/src/excepciones/ManaNegativoException.java
+++ PROG-PersonajesRol/src/excepciones/ManaNegativoException.java
@@ -5,4 +5,9 @@ public class ManaNegativoException extends Exception {
 	public String getMessage() {
 		return "El maná del personaje no puede ser negativo";
 	}
+	
+	@Override
+	public String toString() {
+		return "El maná del personaje no puede ser negativo";
+	}
 }
--- PROG-PersonajesRol/src/excepciones/VidaNegativaException.java
+++ PROG-PersonajesRol/src/excepciones/VidaNegativaException.java
@@ -5,4 +5,9 @@ public class VidaNegativaException extends Exception {
 	public String getMessage() {
 		return "La vida del personaje no puede ser negativa";
 	}
+	
+	@Override
+	public String toString() {
+		return "La vida del personaje no puede ser negativa";
+	}
 }
